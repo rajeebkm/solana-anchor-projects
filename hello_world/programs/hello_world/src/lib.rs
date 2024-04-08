11c1316323b851835eaea1e338fb4a47f74b5a3b
Rajeeb Kumar Malik <rkm2541997@gmail.com>
2024-04-08 17:07:23 +0530

    hello_world added

---
+++ hello_world/programs/hello_world/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("EsPFkJNfvFA5TJ6oN7V6oEtvaR7EAj1iRhHULEVF4XQA");
+
+#[program]
+pub mod hello_world {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
